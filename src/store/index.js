bf5c62c6a2f6ced51e8f02dfa47cc5c1b4efdc31
RayanXmonitoring <evaanjelinaaswitalumbanggaol@gmail.com>
2026-06-17 21:46:19 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import deviceReducer from './slices/deviceSlice';
-import monitoringReducer from './slices/monitoringSlice';
 import adminReducer from './slices/adminSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    devices: deviceReducer,
-    monitoring: monitoringReducer,
     admin: adminReducer,
   },
   middleware: (getDefaultMiddleware) =>
